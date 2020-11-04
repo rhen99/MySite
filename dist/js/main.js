79d3480f003a78a08c8d77a18fdb2652fa22402e
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-11-04 20:16:47 +0800

    mobile done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,5 @@
 const navbar = document.querySelector('.navbar');
+const links = document.querySelectorAll('.links');
 const navigation = document.querySelector('#navigation');
 const menu_btn = document.querySelector('#menu_btn');
 const scroll = new SmoothScroll('a[href*="#"]',{
@@ -23,4 +24,21 @@ menu_btn.addEventListener('click', () => {
 //Load dynamic Year
 window.addEventListener('load', () =>{
     document.querySelector('.year').innerHTML = new Date().getFullYear();
+})
+window.addEventListener('scroll', (e) =>{
+    let fromTop = window.scrollY;
+    links.forEach(link => {
+        const section = document.querySelector(link.hash);
+
+        if(section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight >= fromTop){
+            link.classList.add('active');
+        }else{
+            link.classList.remove('active')
+        }
+
+        
+        
+
+
+    })
 })
