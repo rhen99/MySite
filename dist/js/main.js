7d3c1a26eeeaf0bf4d1f8bdcdcb21fbf2a20bc0f
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-11-05 18:38:33 +0800

    website design done

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,6 +2,10 @@ const navbar = document.querySelector('.navbar');
 const links = document.querySelectorAll('.links');
 const navigation = document.querySelector('#navigation');
 const menu_btn = document.querySelector('#menu_btn');
+const modal_btns = document.querySelectorAll('.contact_us');
+const close_modal = document.querySelector('.close_modal');
+const contact_section = document.querySelector('.contact');
+const contact_modal = document.querySelector('.contact__modal')
 const scroll = new SmoothScroll('a[href*="#"]',{
     speed: 800,
     speedAsDuration: true
@@ -21,24 +25,35 @@ menu_btn.addEventListener('click', () => {
     navigation.classList.toggle('show');
     menu_btn.classList.toggle('opened')
 })
+modal_btns.forEach((btn) => {
+    btn.addEventListener('click', showModal);
+});
+close_modal.addEventListener('click', closeModal);
 //Load dynamic Year
 window.addEventListener('load', () =>{
     document.querySelector('.year').innerHTML = new Date().getFullYear();
 })
-window.addEventListener('scroll', (e) =>{
-    let fromTop = window.scrollY;
-    links.forEach(link => {
-        const section = document.querySelector(link.hash);
 
-        if(section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight >= fromTop){
-            link.classList.add('active');
-        }else{
-            link.classList.remove('active')
-        }
+// window.addEventListener('scroll', (e) =>{
+//     let fromTop = window.scrollY;
+//     links.forEach(link => {
+//         const section = document.querySelector(link.hash);
+//         link.classList.remove('active')
+//         if(section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight >= fromTop){
+//             link.classList.add('active');
+//         }else{
+//             link.classList.remove('active')
+//         }
+//     })
+// })
 
-        
-        
-
-
-    })
-})
+function showModal(e) {
+    e.preventDefault();
+    contact_section.classList.add('show_contact');
+    contact_modal.classList.add('opened_modal')
+}
+function closeModal(e) {
+    e.preventDefault();
+    contact_section.classList.remove('show_contact');
+    contact_modal.classList.remove('opened_modal')
+}
